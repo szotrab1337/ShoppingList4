da9983412ecb6bed337691fcddad94ec8e41b500
szotrab1337 <tazzaq123@gmail.com>
2023-07-22 18:55:52 +0200

    Created local db and added entity framework.

--- ShoppingList4.Api/Program.cs
+++ ShoppingList4.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingList4.Api.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ShoppingListDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ShoppingListDbConnection"));
+});
 
 var app = builder.Build();
 
