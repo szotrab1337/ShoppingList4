d967f55fc780484619d9d0716c7e69bc3636c21f
szotrab1337 <tazzaq123@gmail.com>
2023-07-22 18:38:21 +0200

    Added essential application models.

---
+++ ShoppingList4.Api/Entities/ShoppingList.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingList4.Api.Entities
+{
+    public class ShoppingList
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        public ShoppingList(string name, DateTime createdOn)
+        {
+            Name = name;
+            CreatedOn = createdOn;
+        }
+
+        public virtual List<Entry> Entris { get; set; } = new();
+    }
+}
