f87a89bb139ef4225a3a72b619291dca7584ead4
szotrab1337 <tazzaq123@gmail.com>
2023-08-04 19:44:32 +0200

    Created MAUI project.

---
+++ ShoppingList4.Maui/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.Design;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using ShoppingList4.Maui.ViewModel;
+
+namespace ShoppingList4.Maui
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            Ioc.Default.ConfigureServices(
+                new ServiceCollection()
+                    .AddTransient<MainPageViewModel>()
+                    .BuildServiceProvider());
+
+            return builder.Build();
+        }
+    }
+}
